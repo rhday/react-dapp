b120c6612a373d444aa2284847353d5bdcd28d7b
rhday <54642787+rhday@users.noreply.github.com>
2021-05-27 11:02:29 +0100

    add fetchGreeting function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import {ethers} from 'ethers';
 import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress ="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 function App() {
+  const [greeting, setGreetingValue] = useState();
+
+  async function requestAccount() {
+    
+  }
+
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: data')
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
+  async function setGreeting() {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
